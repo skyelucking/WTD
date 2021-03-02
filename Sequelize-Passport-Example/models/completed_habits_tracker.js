bb327e1d7c300748442950980f4136748f0183f6
kammielatay <kameshialatay@gmail.com>
2021-03-02 15:15:09 -0700

    grabbing user choices for api route

--- Sequelize-Passport-Example/models/completed_habits_tracker.js
+++ Sequelize-Passport-Example/models/completed_habits_tracker.js
@@ -1,24 +1,30 @@
 module.exports = function(sequelize, DataTypes) {
-  const HabitsCompleted = sequelize.define("completed_habits", {
-    completedID: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      allowNull: false,
-      autoIncrement: true
+  const HabitsCompleted = sequelize.define(
+    "completed_habits",
+    {
+      completedID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+      },
+      habitSelectedID: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      userID: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      date: {
+        type: DataTypes.DATE,
+        allowNull: false
+      }
     },
-    habitSelectedID: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    date: {
-      type: DataTypes.DATE,
-      allowNull: false
+    {
+      freezeTableName: true
     }
-  });
+  );
 
   return HabitsCompleted;
 };
--- Sequelize-Passport-Example/models/habits.js
+++ Sequelize-Passport-Example/models/habits.js
@@ -1,24 +1,40 @@
 module.exports = function(sequelize, DataTypes) {
-  const HabitsSelected = sequelize.define("habits_selecteds", {
-    habitID: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      allowNull: false,
-      autoIncrement: true
+  const HabitsSelected = sequelize.define(
+    "habits_selected",
+    {
+      habitID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+      },
+      habitName: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      categoryID: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      userID: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      }
     },
-    habitName: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    cateogyID: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
+    {
+      freezeTableName: true
     }
-  });
+  );
+
+  HabitsSelected.associate = models => {
+    HabitsSelected.hasMany(models.completed_habits, {
+      foreignKey: "userID",
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE"
+    });
+
+    HabitsSelected.belongsTo(models.User, { foreignKey: "userID" });
+  };
 
   return HabitsSelected;
 };
