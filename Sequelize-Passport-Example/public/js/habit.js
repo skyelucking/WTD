0ca0762643e2e8b4258db80cdcffbd18c3a83562
kammielatay <kameshialatay@gmail.com>
2021-03-03 13:41:37 -0700

    generating tracked habits

--- Sequelize-Passport-Example/public/js/habit.js
+++ Sequelize-Passport-Example/public/js/habit.js
@@ -2,18 +2,40 @@ $(document).ready(() => {
   let dataOptions;
   let userId;
   let habits = [];
+  let habitsDisplay = $("<div>").addClass('row text-center');
 
+  // grabbing the user id 
+  $.get("/api/user_data").then(data => {
+    userId = data.id;
+  });
+
+  // getting all the habits from database to render on page
+  $.get("/api/all").then(data => {
+      habits = data;
+      console.log(habits)
+
+    displayHabits(habits); 
+  })
+
+  function displayHabits(arr){
+      arr.forEach((item) => {
+        if(userId === item.userID){
+        let eachHabit = $('<a>').text(item.habitName).addClass('col-3').attr("data-habitID", item.habitID)
+        habitsDisplay.append(eachHabit)
+      }
+      });
+    $("body").append(habitsDisplay);
+  
+  }
+  
+  // grabbing the category they hover over
   $(".category").hover(function() {
     const contentAsset = $(this).attr("data-info");
     dataOptions = JSON.parse(contentAsset);
   });
 
-  $.get("/api/user_data").then(data => {
-    userId = data.id;
-  });
 
-  // making a new object
-  
+  // making a new object for habits 
   $(".option").click(function () {
     let obj;
     obj = {
@@ -21,26 +43,20 @@ $(document).ready(() => {
       categoryID: dataOptions.value,
       userID: userId
     };
-
+    // rendering the items to the page
     $.post("/api/add_habit", obj)
       .then(() => {
-        var row = $("<div>").css("text-align", "center",);
-        let item = $('<div>')
-        row.addClass("habits")
-        item.text(obj.habitName)
-              
-        row.append(item)
-
-        $("body").append(row)
+        let item = $('<a>').text(obj.habitName).addClass('col-3')
+        $(habitsDisplay).append(item)
+        // need to reload page so habitID can generate
+        window.location.reload();
     });
 
   });
 
-  $.get("/api/all").then(data => {
-    habits = data
-    console.log(habits)
-  });
+  // we need to create a route to delete items 
+  // also to prevent them from adding a habit that is already being tracked 
+  // each link should allow them to check if they want to add it to the tracker
+  
 
-
-
-});
+});// end of doc ready
--- Sequelize-Passport-Example/public/js/tracked_habits.js
+++ Sequelize-Passport-Example/public/js/tracked_habits.js
@@ -1,3 +1,48 @@
 $(document).ready(() => {
+  let userId;
+  let habits = [];
+
+
+  $.get("/api/user_data").then(data => {
+    userId = data.id;
+  });
+
+  $.get("/api/all").then(data =>{
+    habits = data;
+    console.log(habits)
+    generateRows(habits)
+  });
+
+  
+  // need a function to create a row into the body of the table
+  function generateRows(arr){
+    arr.forEach(item =>{
+      //table row
+      let tableRow = $("<tr>")
+      $("tbody").append(tableRow);
+      // create table head
+      let tableHead = $("<th>").attr("scope", "row").text(item.habitName)
+      tableRow.append(tableHead);
+
+      // create table data
+      
+      for (let i = 0; i < 7; i++){
+        let count = i; 
+        let tableData = $("<td>");
+        let form = $("<div>").addClass("form-check")
+        let input = $("<input>").addClass("form-check-input").prop("type", "checkbox");
+        
+        form.append(input)
+        tableData.append(form)
+        tableRow.append(tableData)
+
+        count++;
+      }
+  
+    })
+  }
+
+
+
 
 }); // end of export
