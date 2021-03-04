c2d0fd156f8d5d740da2b0d728008ce574549fe4
kammielatay <kameshialatay@gmail.com>
2021-03-03 19:25:50 -0700

    giving you updated files

--- Sequelize-Passport-Example/public/js/tracked_habits.js
+++ Sequelize-Passport-Example/public/js/tracked_habits.js
@@ -17,28 +17,29 @@ $(document).ready(() => {
   // need a function to create a row into the body of the table
   function generateRows(arr){
     arr.forEach(item => {
-      //table row
-      let tableRow = $("<tr>")
-      $("tbody").append(tableRow);
+        if (userId === item.userID){
+        //table row
+        let tableRow = $("<tr>")
+        $("tbody").append(tableRow);
 
-      // create table head
-      let tableHead = $("<th>").attr("scope", "row").text(item.habitName)
-      tableRow.append(tableHead);
+        // create table head
+        let tableHead = $("<th>").attr("scope", "row").text(item.habitName)
+        tableRow.append(tableHead);
 
-      // create table data
-      for (let i = 0; i < 7; i++){
-        let count = i; 
-        let tableData = $("<td>");
-        let form = $("<div>").addClass("form-check")
-        let input = $("<input>").addClass("form-check-input").prop("type", "checkbox");
-        
-        form.append(input)
-        tableData.append(form)
-        tableRow.append(tableData)
-        count++;
+        // create table data
+        for (let i = 0; i < 7; i++) {
+          let count = i;
+          let tableData = $("<td>");
+          let form = $("<div>").addClass("form-check")
+          let input = $("<input>").addClass("form-check-input").prop("type", "checkbox");
+
+          form.append(input)
+          tableData.append(form)
+          tableRow.append(tableData)
+          count++;
+        }
       }
-  
-    })
+    }) 
   } // end of function
 
   function generateMarkedComplete(arr){
