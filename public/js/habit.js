5f942d7104487cd38ea3a0997906fbd6b61801ff
Skye Lucking <lucking.skye@gmail.com>
2021-03-07 18:18:43 -0700

    added the refresh week button and functionality to clear all the check boxes

--- public/js/habit.js
+++ public/js/habit.js
@@ -2,15 +2,16 @@ $(document).ready(() => {
   let dataOptions;
   let userId;
   let habits = [];
-   
-  let habitsDisplay = $("<div>").addClass("container text-center ")
-  .css("width", "90%")
-  .css("padding", "0px")
-  .css("align-content", "left")
-  .css("border-style", "solid")
-  .css( "border-color", "white")
-  .css("color", "#D99830")
-  .css( "margin-bottom", "20px");
+
+  let habitsDisplay = $("<div>")
+    .addClass("container text-center ")
+    .css("width", "90%")
+    .css("padding", "0px")
+    .css("align-content", "left")
+    .css("border-style", "solid")
+    .css("border-color", "white")
+    .css("color", "#D99830")
+    .css("margin-bottom", "20px");
 
   let addAHabit = $("#addAHabit");
 
@@ -23,44 +24,37 @@ $(document).ready(() => {
 
   function displayHabits(arr) {
     habitsDisplay.empty();
-        
-    let listHeader = $("<div>")
-    .html('<img src="./img/list_header2.png" text-align="center" width="100%" margin="0px" padding="0px">');
-        
+
+    let listHeader = $("<div>").html(
+      '<img src="./img/list_header2.png" text-align="center" width="100%" margin="0px" padding="0px">'
+    );
+
     arr.forEach((item) => {
       if (userId === item.userID) {
-        
         let eachHabit = $("<div>")
           .text(item.habitName)
           .attr("data-habitID", item.habitID)
-          .css("width", "100%", )
-          .css( "background-color", "white")
+          .css("width", "100%")
+          .css("background-color", "white")
           .css("font-family", "Quicksand")
-          .css("font-size", "1.25em", )
-          .css("padding", "0px", )
-          .css("margin", "0px", )
-          ;
-        
-          let deleteHabit= $("<div>")
+          .css("font-size", "1.25em")
+          .css("padding", "0px")
+          .css("margin", "0px");
+        let deleteHabit = $("<div>")
           .html('<i class="fas fa-trash-alt"></i>')
           .addClass("btn")
           .css("color", "#DDA797")
           .attr("data-habitID", item.habitID);
-          
-         
 
-          deleteHabit.click(function () {
+        deleteHabit.click(function() {
           $.ajax({
             url: "api/delete_habit",
             type: "DELETE",
             data: { habitID: item.habitID },
             dataType: "json",
-
-          })
-            .always(function () {
-              renderHabits();
-            }
-            );
+          }).always(function() {
+            renderHabits();
+          });
         });
         habitsDisplay.append(eachHabit);
         eachHabit.append(deleteHabit).append("<hr>");
@@ -79,15 +73,13 @@ $(document).ready(() => {
   }
 
   // grabbing the category they hover over
-  $(".category").hover(function () {
+  $(".category").hover(function() {
     const contentAsset = $(this).attr("data-info");
     dataOptions = JSON.parse(contentAsset);
   });
 
-
   // making a new object for habits
-  $(".option").click(function () {
-
+  $(".option").click(function() {
     let obj;
     obj = {
       habitName: $(this).attr("data-habit-option"),
@@ -108,11 +100,12 @@ $(document).ready(() => {
     });
   });
 
- 
-  $("#addCustomHabit").on('click', function () {
+  $("#addCustomHabit").on("click", function() {
     let customHabit;
     customHabit = {
-      habitName: $("#inputHabit").val().trim(),
+      habitName: $("#inputHabit")
+        .val()
+        .trim(),
       categoryID: 7,
       userID: userId,
       Monday: true,
@@ -122,22 +115,15 @@ $(document).ready(() => {
       Friday: true,
       Saturday: true,
       Sunday: true,
-      };
+    };
 
     if (!customHabit) {
       return;
     }
-   
+
     $.post("/api/add_habit", customHabit).then(() => {
-      console.log(customHabit)
+      console.log(customHabit);
       renderHabits();
-    });     
-    
     });
-
-
-
-
-
+  });
 }); // end of doc ready
-
